39d6893abc91637202ddec8ae75554ba1662a2a0
ElliotEserin <ellioteserin@users.noreply.github.com>
2020-03-03 16:31:17 +0000

    Commit after adding basic UI mechanics and working on scene

---
+++ Anthropocene FMP/Assets/GameUI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameUI : MonoBehaviour
+{
+    public TextMeshProUGUI oxygenText;
+    public TextMeshProUGUI foodText;
+    public TextMeshProUGUI waterText;
+
+    PlayerManager pm;
+
+    private void Start()
+    {
+        pm = FindObjectOfType<PlayerManager>();
+    }
+
+    private void Update()
+    {
+        oxygenText.text = "Oxygen: " + Math.Round(pm.oxygen);
+        foodText.text = "Food: " + Math.Round(pm.food);
+        waterText.text = "Water: " + Math.Round(pm.water);
+    }
+}
---
+++ Anthropocene FMP/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float oxygen = 100f;
+    float rateOfOxygenDecrease = 0.4f;
+    public float food = 100f;
+    float rateOfFoodDecrease = 0.3f;
+    public float water = 100f;
+    float rateOfWaterDecrease = 0.2f;
+
+    void Update()
+    {
+        oxygen -= rateOfOxygenDecrease * Time.deltaTime;
+        food -= rateOfFoodDecrease * Time.deltaTime;
+        water -= rateOfWaterDecrease * Time.deltaTime;
+    }
+}
